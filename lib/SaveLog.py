587d16e463b52768d9d70bbbc129fdcadeff1847
Lei Gao <lei.gao@innocharging.com>
2021-11-11 11:29:06 +0800

    add test logs

--- lib/SaveLog.py
+++ lib/SaveLog.py
@@ -6,7 +6,10 @@
 
 import logging
 import os
-from lib import GetTime
+import sys
+import io
+from datetime import datetime
+from FOTA.lib import GetTime
 
 class save_log:
     def __init__(self):
@@ -34,5 +37,40 @@ class save_log:
                             # 日志格式
                             )
 
+
+    def create_detail_day(self):
+        nowtime = datetime.now()
+        daytime = nowtime.strftime('log_'+'%Y%m%d'+'_'+'%H%M%S')
+        return daytime
+
+    def make_print_to_file(self,path='./'):
+        class Logger(object):
+            # print(filename)
+            def __init__(self,filename="Default.log",path="./"):
+                sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+                self.terminal = sys.stdout
+                print("path:", path)
+                print("filename:",filename)
+                self.log = open(os.path.join(path,filename),"a",encoding='utf-8')
+
+            def write(self,message):
+                self.terminal.write(message)
+                self.log.write(message)
+
+            def flush(self):
+                pass
+
+        sys.stdout = Logger(self.create_detail_day() + '.txt', path=path)
+        print(self.create_detail_day().center(60,'*'))
+
 if __name__ == '__main__':
-    save_log().log_to_file()
+    # detail_time = save_log().create_detail_day()
+    # print("detail_time:",detail_time)
+    save_log().make_print_to_file(path = "C:\\08_Robot\\FOTA_NEW\\log")
+
+    print('explanation'.center(80,'*'))
+    info1 = "从小到大排序"
+    info2 = "sort the form large to samll"
+    print(info1)
+    print(info2)
+    print('END: explation'.center(80,'*'))
